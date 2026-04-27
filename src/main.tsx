2203e71b4383a0ddf292d9e9f86e9b2bc54dd566
unknown <dntelbron5@gmail.com>
2026-04-27 12:24:11 +0800

    fix 404 on refresh

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster position="top-right" />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
 
